e4d61e3fc6b848c6d85feb91e2f0042fc722f8de
massiliatazit <massiliatazit@users.noreply.github.com>
2020-12-18 16:06:26 +0100

    reviews works

--- src/reviews/index.js
+++ src/reviews/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { join } = require("path");
 const fse = require("fs-extra");
+const { check } = require("express-validator");
 const uniqid = require("uniqid");
+const {getmovies,writemovies} = require("../fsUtilities")
 const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
-const reviewsPath = join(__dirname, "reviews.json");
-
+const reviewsFilePath = join(__dirname, "reviews.json")
+const moviesFilePath = join(__dirname, "../media/media.json")
 //GET method
 router.get("/", async (req, res) => {
   let reviewsFile = await fse.readJSON(reviewsPath);
@@ -22,26 +24,64 @@ router.get("/:id", async (req, res) => {
 
 // POST method
 router.post(
-  "/",
-  [
-    body("comment").exists(),
-    body("rate").isInt({ max: 5 }),
-    body("elementId").exists(),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    } else {
-      let newReview = { _id: uniqid(), ...req.body, createdAt: new Date() };
-      let reviewsFile = await fse.readJSON(reviewsPath);
-      reviewsFile.push(newReview);
+    "/",
+    
+      check("comment")
+        .exists()
+        .isLength({ min: 1 })
+        .withMessage("Give it a comment please :) "),
+      check("rate")
+        .exists()
+        .isLength({ min: 1 })
+        .isInt({min:1,max:5})
+        // .custom((val, { req }) => {
+        //     console.log('here')
+        //     if ( parseInt(req.body.rate) > 5) {
+        //       throw new Error('The rate should be max 5');
+        //     }
+        // })
+        .withMessage("You have to rate it at be kind give 5 :D"),
      
-      await fse.writeJSON(reviewsPath, reviewsFile);
-      res.send("Posted Sucessfully");
-    }
-  }
-);
+      check("elementId")
+        .exists()
+        .isLength({ min: 1 })
+        .custom(async(val,{req})=>{
+       
+          const moviesDB = await getmovies(moviesFilePath)
+            if(!moviesDB.find(movie=> movie.imdbID===req.body.elementId)) {
+                throw new Error("The movie id doesn't exists ") 
+        }})
+        .withMessage("You need to have your movie ID"),
+    
+    async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        
+        const err = {};
+        err.message = errors;
+        err.httpStatusCode = 400;
+        next(err);
+      } else {
+        const reviewsDB = await getmovies(reviewsFilePath); 
+         const moviesDB = await getmovies(moviesFilePath)
+         console.log("movies data",moviesDB)
+         if (moviesDB.find(movie=> movie.imdbID===req.body.elementId)) {
+             const newreview = req.body;
+        
+        newreview.ID = uniqid(); 
+       
+        newreview.CreationDate = new Date(); 
+        reviewsDB.push(newreview); 
+         }
+        await writemovies(reviewsFilePath, reviewsDB); 
+        res.status(201).send(reviewsDB); 
+          }
+
+
+        
+      }
+    
+  );
 
 //PUT method
 router.put("/:id", async (req, res) => {
